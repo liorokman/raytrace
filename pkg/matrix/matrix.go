8defe25a24b503421348aa71bb9315af3dcc74c6
Lior Okman <lior.okman@gmail.com>
2019-11-15 20:50:42 +0200

    Finished chapter 3.

--- pkg/matrix/matrix.go
+++ pkg/matrix/matrix.go
@@ -1,8 +1,10 @@
 package matrix
 
 import (
+	"fmt"
 	"github.com/liorokman/raytrace/pkg/tuple"
 	"github.com/liorokman/raytrace/pkg/utils"
+	"math"
 )
 
 type Matrix [][]float64
@@ -52,4 +54,68 @@ func (l Matrix) MultiplyTuple(r tuple.Tuple) tuple.Tuple {
 	}
 	result := l.Multiply(right)
 	return tuple.Tuple{result[0][0], result[1][0], result[2][0], result[3][0]}
+}
+
+func (l Matrix) Transpose() Matrix {
+	result := New(len(l), len(l[0]))
+	for i := range l {
+		for j := range l[0] {
+			result[i][j] = l[j][i]
+		}
+	}
+	return result
+}
+
+func (l Matrix) Submatrix(row, col int) Matrix {
+
+	result := New(len(l)-1, len(l[0])-1)
+
+	for index := range l {
+		realI := index
+		switch {
+		case index == row:
+			continue
+		case index > row:
+			realI = realI - 1
+		}
+		copy(result[realI][0:col], l[index][0:col])
+		copy(result[realI][col:], l[index][col+1:])
+	}
+
+	return result
+}
+
+func (m Matrix) Determinant() float64 {
+	if len(m) == 2 && len(m[0]) == 2 {
+		return m[0][0]*m[1][1] - m[0][1]*m[1][0]
+	}
+	det := 0.0
+	for col := range m[0] {
+		det = det + m[0][col]*m.Cofactor(0, col)
+	}
+	return det
+}
+
+func (m Matrix) Minor(row, col int) float64 {
+	return m.Submatrix(row, col).Determinant()
+}
+
+func (m Matrix) Cofactor(row, col int) float64 {
+	return math.Pow(-1, float64(row+col)) * m.Minor(row, col)
+}
+
+func (m Matrix) Inverse() (Matrix, error) {
+	d := m.Determinant()
+	if utils.FloatEqual(d, 0.0) {
+		return m, fmt.Errorf("Matrix is not invertable")
+	}
+	result := New(len(m), len(m[0]))
+	for row := range m {
+		for col := range m[0] {
+			c := m.Cofactor(row, col)
+			result[col][row] = c / d
+		}
+
+	}
+	return result, nil
 }
--- pkg/matrix/matrix_test.go
+++ pkg/matrix/matrix_test.go
@@ -105,3 +105,150 @@ func TestMultiplyTuple(t *testing.T) {
 	r1 := tuple.Tuple{18, 24, 33, 1}
 	g.Expect(m1.MultiplyTuple(t1)).To(Equal(r1))
 }
+
+func TestTranspose(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	m1 := Matrix{
+		[]float64{0, 9, 3, 0},
+		[]float64{9, 8, 0, 8},
+		[]float64{1, 8, 5, 3},
+		[]float64{0, 0, 5, 8},
+	}
+
+	m2 := Matrix{
+		[]float64{0, 9, 1, 0},
+		[]float64{9, 8, 8, 0},
+		[]float64{3, 0, 5, 5},
+		[]float64{0, 8, 3, 8},
+	}
+
+	g.Expect(m1.Transpose().Equals(m2)).To(BeTrue())
+}
+
+func TestSubmatrix(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	m1 := Matrix{
+		[]float64{1, 5, 0},
+		[]float64{-3, 2, 7},
+		[]float64{0, 6, 3},
+	}
+
+	g.Expect(m1.Submatrix(0, 2).Equals(Matrix{{-3, 2}, {0, 6}})).To(BeTrue())
+	g.Expect(m1.Submatrix(1, 1).Equals(Matrix{{1, 0}, {0, 3}})).To(BeTrue())
+	g.Expect(m1.Submatrix(0, 0).Equals(Matrix{{2, 7}, {6, 3}})).To(BeTrue())
+
+	m2 := Matrix{
+		[]float64{-6, 1, 1, 6},
+		[]float64{-8, 5, 8, 6},
+		[]float64{-1, 0, 8, 2},
+		[]float64{-7, 1, -1, 1},
+	}
+	g.Expect(m2.Submatrix(2, 1).Equals(Matrix{{-6, 1, 6}, {-8, 8, 6}, {-7, -1, 1}})).To(BeTrue())
+}
+
+func TestMinor(t *testing.T) {
+	g := NewGomegaWithT(t)
+	m := Matrix{
+		{3, 5, 0},
+		{2, -1, -7},
+		{6, -1, 5},
+	}
+	g.Expect(m.Minor(1, 0)).To(Equal(25.0))
+	g.Expect(m.Minor(0, 0)).To(Equal(-12.0))
+	g.Expect(m.Minor(1, 1)).To(Equal(15.0))
+	g.Expect(m.Minor(2, 2)).To(Equal(-13.0))
+}
+
+func TestCofactor(t *testing.T) {
+	g := NewGomegaWithT(t)
+	m := Matrix{
+		{3, 5, 0},
+		{2, -1, -7},
+		{6, -1, 5},
+	}
+	g.Expect(m.Cofactor(0, 0)).To(Equal(-12.0))
+	g.Expect(m.Cofactor(1, 0)).To(Equal(-25.0))
+}
+
+func TestDeterminant(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	m := Matrix{
+		{1, 2, 6},
+		{-5, 8, -4},
+		{2, 6, 4},
+	}
+	g.Expect(m.Cofactor(0, 0)).To(Equal(56.0))
+	g.Expect(m.Cofactor(0, 1)).To(Equal(12.0))
+	g.Expect(m.Cofactor(0, 2)).To(Equal(-46.0))
+	g.Expect(m.Determinant()).To(Equal(-196.0))
+
+	m = Matrix{
+		{-2, -8, 3, 5},
+		{-3, 1, 7, 3},
+		{1, 2, -9, 6},
+		{-6, 7, 7, -9},
+	}
+	g.Expect(m.Cofactor(0, 0)).To(Equal(690.0))
+	g.Expect(m.Cofactor(0, 1)).To(Equal(447.0))
+	g.Expect(m.Cofactor(0, 2)).To(Equal(210.0))
+	g.Expect(m.Cofactor(0, 3)).To(Equal(51.0))
+	g.Expect(m.Determinant()).To(Equal(-4071.0))
+}
+
+func TestInverse(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	m := Matrix{
+		{8, -5, 9, 2},
+		{7, 5, 6, 1},
+		{-6, 0, 9, 6},
+		{-3, 0, -9, -4},
+	}
+
+	invM := Matrix{
+		{-0.15385, -0.15385, -0.28205, -0.53846},
+		{-0.07692, 0.12308, 0.02564, 0.03077},
+		{0.35897, 0.35897, 0.43590, 0.92308},
+		{-0.69231, -0.69231, -0.76923, -1.92308},
+	}
+	calcInv, err := m.Inverse()
+	g.Expect(err).To(BeNil())
+	g.Expect(calcInv.Equals(invM)).To(BeTrue())
+
+	m = Matrix{
+		{9, 3, 0, 9},
+		{-5, -2, -6, -3},
+		{-4, 9, 6, 4},
+		{-7, 6, 6, 2},
+	}
+	invM = Matrix{
+		{-0.04074, -0.07778, 0.14444, -0.22222},
+		{-0.07778, 0.03333, 0.36667, -0.33333},
+		{-0.02901, -0.14630, -0.10926, 0.12963},
+		{0.17778, 0.06667, -0.26667, 0.33333},
+	}
+	calcInv, err = m.Inverse()
+	g.Expect(err).To(BeNil())
+	g.Expect(calcInv.Equals(invM)).To(BeTrue())
+
+	a := Matrix{
+		{3, -9, 7, 3},
+		{3, -8, 2, -9},
+		{-4, 4, 4, 1},
+		{-6, 5, -1, 1},
+	}
+	b := Matrix{
+		{8, 2, 2, 2},
+		{3, -1, 7, 0},
+		{7, 0, 5, 4},
+		{6, -2, 0, 5},
+	}
+
+	c := a.Multiply(b)
+	invB, err := b.Inverse()
+	g.Expect(err).To(BeNil())
+	g.Expect(c.Multiply(invB).Equals(a)).To(BeTrue())
+}
